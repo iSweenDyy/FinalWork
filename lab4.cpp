b7e88ed5c3a48a3dbe22c5b20c88bc972c41750b
h3xvenly <denchik.maxx2007@mail.ru>
2026-04-14 14:36:12 +0300

    update cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,15 +1,35 @@
 #include <stdio.h>
+
 int main() {
     int a1;
     int b;
     int c;
+    
     a1 = 5;
     b = 10;
     c = a1 + b;
-    // проверка: является ли переменная a1 положительной
+    
+
     if (a1 > 0) {
         printf("Переменная a1 положительная\n");
     }
+    
     printf("%d\n", c);
+    
+
+    double arr[30];
+    
+
+    for (int i = 0; i < 30; i++) {
+        arr[i] = i;
+    }
+    
+
+    printf("Массив из 30 элементов:\n");
+    for (int i = 0; i < 30; i++) {
+        printf("%.1f ", arr[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
