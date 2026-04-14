48a2653ea4a4e2080f8b118e98c180f03df41152
iSweenDyy <ilysov086@gmail.com>
2026-04-14 14:58:42 +0300

    add sin() on cpp file

--- lab4.cpp
+++ lab4.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>  
 
 int main() {
     int a1;
@@ -9,25 +10,25 @@ int main() {
     b = 10;
     c = a1 + b;
     
-
     if (a1 > 0) {
         printf("Переменная a1 положительная\n");
     }
     
     printf("%d\n", c);
     
-
-    double arr[30];
+    double arr[30];  
     
-
+    
+    printf("значения sin ");
     for (int i = 0; i < 30; i++) {
-        arr[i] = i;
+        arr[i] = sin(i);  
+        printf("Итерация %d: sin(%d) = %.4f\n", i, i, arr[i]); 
     }
     
-
-    printf("Массив из 30 элементов:\n");
+  
+    printf("\nМассив в строку:\n");
     for (int i = 0; i < 30; i++) {
-        printf("%.1f ", arr[i]);
+        printf("%.4f ", arr[i]);
     }
     printf("\n");
     
